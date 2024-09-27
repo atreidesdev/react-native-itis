4c89eb7675c3080cca88f9037ed02a4f58e9d3c0
Дмитрий Моисеенко <lovecloudez@gmail.com>
2024-09-27 17:59:34 +0300

    init project

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,11 @@
+import { ScrollView, Text } from 'react-native';
+
+export default function HomeScreen() {
+    return (
+        <ScrollView>
+            <Text>init</Text>
+        </ScrollView>
+    );
+}
+
+
