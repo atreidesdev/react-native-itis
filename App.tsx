808ef63de2d0dd1c8d7da1f38fd094f6b21460b3
Дмитрий Моисеенко <lovecloudez@gmail.com>
2024-10-08 14:27:14 +0300

    init

---
+++ App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+} from 'react-native';
+
+function App(): React.JSX.Element {
+
+  return (
+    <SafeAreaView>
+      <Text>init</Text>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+});
+
+export default App;
